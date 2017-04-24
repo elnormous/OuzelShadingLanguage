1c70f7abb603057732758d9fa257b43d54697fc3
Elviss Strazdins <elviss@elviss.lv>
2017-04-24 23:15:12 +0300

    Initial Commit

---
+++ OSLParser/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OSLParser
+//
+//  Created by Elviss Strazdins on 24/04/2017.
+//  Copyright © 2017 Elviss Strazdins. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
