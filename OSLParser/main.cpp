c285097f831792cce415e9f36e452a740f99a538
Elviss Strazdins <elviss@elviss.lv>
2017-04-25 15:35:48 +0300

    Add typedef keyword

--- OSLParser/main.cpp
+++ OSLParser/main.cpp
@@ -36,6 +36,7 @@ struct Token
         KEYWORD_FALSE, // false
         KEYWORD_INLINE, // inline
         KEYWORD_STRUCT, // struct
+        KEYWORD_TYPEDEF, // typedef
         LEFT_PARENTHESIS, // )
         RIGHT_PARENTHESIS, // (
         LEFT_BRACE, // {
@@ -187,6 +188,7 @@ bool tokenize(const std::vector<uint8_t>& buffer, std::vector<Token>& tokens)
             else if (token.value == "false") token.type = Token::Type::KEYWORD_FALSE;
             else if (token.value == "inline") token.type = Token::Type::KEYWORD_INLINE;
             else if (token.value == "struct") token.type = Token::Type::KEYWORD_STRUCT;
+            else if (token.value == "typedef") token.type = Token::Type::KEYWORD_TYPEDEF;
             else token.type = Token::Type::IDENTIFIER;
         }
         else if (c == '+' || c == '-' ||
@@ -628,6 +630,7 @@ int main(int argc, const char * argv[])
             case Token::Type::KEYWORD_FALSE: std::cout << "KEYWORD_FALSE"; break;
             case Token::Type::KEYWORD_INLINE: std::cout << "KEYWORD_INLINE"; break;
             case Token::Type::KEYWORD_STRUCT: std::cout << "KEYWORD_STRUCT"; break;
+            case Token::Type::KEYWORD_TYPEDEF: std::cout << "KEYWORD_TYPEDEF"; break;
             case Token::Type::LEFT_PARENTHESIS: std::cout << "LEFT_PARENTHESIS"; break;
             case Token::Type::RIGHT_PARENTHESIS: std::cout << "RIGHT_PARENTHESIS"; break;
             case Token::Type::LEFT_BRACE: std::cout << "LEFT_BRACE"; break;
