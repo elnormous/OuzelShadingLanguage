0ac6c406df291bfa5045641a83c5f8e0c6d298ab
Elviss Strazdins <elviss@elviss.lv>
2019-09-16 01:56:30 +0300

    Add test for for statement

--- test/main.cpp
+++ test/main.cpp
@@ -253,15 +253,15 @@ static void testDoStatement()
 
     if (!statement ||
         statement->getStatementKind() != Statement::Kind::Do)
-        throw std::runtime_error("Expected a while statement");
+        throw std::runtime_error("Expected a do statement");
 
-    auto whileStatement = static_cast<const WhileStatement*>(statement);
+    auto doStatement = static_cast<const DoStatement*>(statement);
 
-    if (!whileStatement->condition ||
-        whileStatement->condition->getKind() != Construct::Kind::Expression)
+    if (!doStatement->condition ||
+        doStatement->condition->getKind() != Construct::Kind::Expression)
         throw std::runtime_error("Expected an expression condition");
 
-    auto condition = static_cast<const Expression*>(whileStatement->condition);
+    auto condition = static_cast<const Expression*>(doStatement->condition);
 
     if (condition->getExpressionKind() != Expression::Kind::Literal)
         throw std::runtime_error("Expected a literal condition");
@@ -276,8 +276,63 @@ static void testDoStatement()
     if (boolLiteralCondition->value != true)
         throw std::runtime_error("Expected a literal with a value \"true\"");
 
-    if (!whileStatement->body ||
-        whileStatement->body->getStatementKind() != Statement::Kind::Compound)
+    if (!doStatement->body ||
+        doStatement->body->getStatementKind() != Statement::Kind::Compound)
+        throw std::runtime_error("Expected a compound statement");
+}
+
+static void testForStatement()
+{
+    std::string code = R"OSL(
+    void main()
+    {
+        for (; true;)
+        {
+        }
+    }
+    )OSL";
+
+    std::vector<Token> tokens = tokenize(code);
+    ASTContext context(tokens);
+
+    auto topDeclarations = context.getDeclarations();
+    if (topDeclarations.size() != 1)
+        throw std::runtime_error("Expected the main function");
+
+    auto mainCompoundStatement = getMainBody(topDeclarations.front());
+
+    if (mainCompoundStatement->statements.size() != 1)
+        throw std::runtime_error("Expected a statement");
+
+    auto statement = mainCompoundStatement->statements.front();
+
+    if (!statement ||
+        statement->getStatementKind() != Statement::Kind::For)
+        throw std::runtime_error("Expected a for statement");
+
+    auto forStatement = static_cast<const ForStatement*>(statement);
+
+    if (!forStatement->condition ||
+        forStatement->condition->getKind() != Construct::Kind::Expression)
+        throw std::runtime_error("Expected an expression condition");
+
+    auto condition = static_cast<const Expression*>(forStatement->condition);
+
+    if (condition->getExpressionKind() != Expression::Kind::Literal)
+        throw std::runtime_error("Expected a literal condition");
+
+    auto literalCondition = static_cast<const LiteralExpression*>(condition);
+
+    if (literalCondition->getLiteralKind() != LiteralExpression::Kind::Boolean)
+        throw std::runtime_error("Expected a bool literal condition");
+
+    auto boolLiteralCondition = static_cast<const BooleanLiteralExpression*>(literalCondition);
+
+    if (boolLiteralCondition->value != true)
+        throw std::runtime_error("Expected a literal with a value \"true\"");
+
+    if (!forStatement->body ||
+        forStatement->body->getStatementKind() != Statement::Kind::Compound)
         throw std::runtime_error("Expected a compound statement");
 }
 
@@ -290,6 +345,7 @@ int main(int argc, const char * argv[])
         testIfStatement();
         testWhileStatement();
         testDoStatement();
+        testForStatement();
     }
     catch (const std::exception& e)
     {
