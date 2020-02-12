4fddf198094472b711c80fce29980a0338949634
Elviss Strazdins <elviss@elviss.lv>
2020-02-12 19:41:27 +0200

    Add classes for attributes

--- osl/Attributes.hpp
+++ osl/Attributes.hpp
@@ -45,7 +45,10 @@ namespace ouzel
             PositionTransformed,
             PointSize,
             Tangent,
-            TextureCoordinates
+            TextureCoordinates,
+            In,
+            Inout,
+            Out
         };
 
         Attribute(Kind initAttributeKind): Construct(Construct::Kind::Attribute), attributeKind(initAttributeKind) {}
@@ -55,6 +58,97 @@ namespace ouzel
     protected:
         const Kind attributeKind;
     };
+
+    class FragmentAttribute: public Attribute
+    {
+    public:
+        FragmentAttribute(): Attribute(Attribute::Kind::Fragment) {}
+    };
+
+    class VertexAttribute: public Attribute
+    {
+    public:
+        VertexAttribute(): Attribute(Attribute::Kind::Vertex) {}
+    };
+
+    class BinormalAttribute: public Attribute
+    {
+    public:
+        BinormalAttribute(): Attribute(Attribute::Kind::Binormal) {}
+    };
+
+    class BlendIndicesAttribute: public Attribute
+    {
+    public:
+        BlendIndicesAttribute(): Attribute(Attribute::Kind::BlendIndices) {}
+    };
+
+    class BlendWeightAttribute: public Attribute
+    {
+    public:
+        BlendWeightAttribute(): Attribute(Attribute::Kind::BlendWeight) {}
+    };
+
+    class ColorAttribute: public Attribute
+    {
+    public:
+        ColorAttribute(): Attribute(Attribute::Kind::Color) {}
+    };
+
+    class NormalAttribute: public Attribute
+    {
+    public:
+        NormalAttribute(): Attribute(Attribute::Kind::Normal) {}
+    };
+
+    class PositionAttribute: public Attribute
+    {
+    public:
+        PositionAttribute(): Attribute(Attribute::Kind::Position) {}
+    };
+
+    class PositionTransformedAttribute: public Attribute
+    {
+    public:
+        PositionTransformedAttribute(): Attribute(Attribute::Kind::PositionTransformed) {}
+    };
+
+    class PointSizeAttribute: public Attribute
+    {
+    public:
+        PointSizeAttribute(): Attribute(Attribute::Kind::PointSize) {}
+    };
+
+    class TangentAttribute: public Attribute
+    {
+    public:
+        TangentAttribute(): Attribute(Attribute::Kind::Tangent) {}
+    };
+
+    class TextureCoordinatesAttribute: public Attribute
+    {
+    public:
+        TextureCoordinatesAttribute(): Attribute(Attribute::Kind::TextureCoordinates) {}
+    };
+
+    class InAttribute: public Attribute
+    {
+    public:
+        InAttribute(): Attribute(Attribute::Kind::In) {}
+    };
+
+    class InoutAttribute: public Attribute
+    {
+    public:
+        InoutAttribute(): Attribute(Attribute::Kind::Inout) {}
+    };
+
+    class OutAttribute: public Attribute
+    {
+    public:
+        OutAttribute(): Attribute(Attribute::Kind::Out) {}
+    };
+
 }
 
 #endif // ATTRIBUTES_HPP
